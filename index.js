b080bb3f35dd83796703f1d4ee02e43a5d066210
david collett <davida.collett95@gmail.com>
2019-07-25 16:28:53 -0600

    finished each step except black diamond

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const products_controller = require('./products_controller')
+
+const app = express()
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env
+
+app.use(express.json())
+
+massive(CONNECTION_STRING)
+.then((dbinstance) => {
+    app.set('db', dbinstance)
+})
+.catch(err => {
+    console.log(err)
+})
+
+app.post(`/api/products`, products_controller.create)
+app.get(`/api/products`, products_controller.getAll )
+app.get('/api/products/:id', products_controller.getOne)
+app.put(`/api/products/:id`, products_controller.update)
+app.delete(`/api/products/:id`, products_controller.delete)
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`There are ${SERVER_PORT} angry sloths in the backyard. `)
+})
